6d6db9ce79321a6b695a2730ef39e38c1dde4962
CianTivnan <cian.tivnan@yahoo.ie>
2021-03-16 15:48:17 +0000

    firebase data added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC1ngfgYCzJgx_uC1SNFtLNIzoYdmY6wWI",
+    authDomain: "carsales-ba4d6.firebaseapp.com",
+    projectId: "carsales-ba4d6",
+    storageBucket: "carsales-ba4d6.appspot.com",
+    messagingSenderId: "688074186997",
+    appId: "1:688074186997:web:d0c13d5563aa38d2b4e5bc"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC1ngfgYCzJgx_uC1SNFtLNIzoYdmY6wWI",
+    authDomain: "carsales-ba4d6.firebaseapp.com",
+    projectId: "carsales-ba4d6",
+    storageBucket: "carsales-ba4d6.appspot.com",
+    messagingSenderId: "688074186997",
+    appId: "1:688074186997:web:d0c13d5563aa38d2b4e5bc"
+  }
 };
 
 /*
